19fa54304b585658626dce44dcf717a0b741668b
Dopha-mipa <puremint09@gmail.com>
2017-06-20 01:34:44 +0900

    .

--- Algorithm/AI/02_DFS.cc
+++ Algorithm/AI/02_DFS.cc
@@ -14,11 +14,10 @@ int main() {
   int N;
   cout << "Enter the number of Queens : ";
   cin >> N;
+
   int board[N];
   int ans_cnt = 0;
-  cout << "시작 : " << ans_cnt << endl;
 
-  // (0, 0)에 퀸을 두고 시작
   for (int i = 0; i < N; i++) {
     board[0] = i;
     queens(0, board, N, 0, ans_cnt);
@@ -29,28 +28,19 @@ int main() {
   } else {
     cout << "정답의 수 : " << ans_cnt << endl;
   }
+
   return 0;
 }
 
-// 이중배열이라면 int (*board)[N] 처럼
 void queens(int index, int board[], int N, int depth, int &ans_cnt) {
   int new_Q;
 
-  // 중간 점검용
-  // for (int i = 0; i < depth; i++) {
-    // cout << "    ";
-  // }
-  // cout << "queens " << index << " " << board[index] << " "<< depth << endl;
-
   if (promising(index, board)) {
     if (index == N - 1) {
-      /* 출력하는 경우
-      cout << "===========Answer==========" << endl;
       for (int i = 0; i < N; i++) {
-        cout << board[i] << " ";
+        cout << board[i] << "  ";
       }
       cout << endl;
-      */
       ans_cnt += 1;
     }
     else {
@@ -60,7 +50,6 @@ void queens(int index, int board[], int N, int depth, int &ans_cnt) {
       }
     }
   } // end if promising
-  // return;
 } 
 
 bool promising(int new_Q, int board[]) {
@@ -75,6 +64,6 @@ bool promising(int new_Q, int board[]) {
     }
     k++;
   }
-  // cout << "flag : " << flag << " new_Q : " << new_Q << endl;
+
   return flag;
 }
